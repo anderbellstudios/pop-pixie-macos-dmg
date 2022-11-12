a480180405a920c2d4a927cb911fbafea933f16f
Joe Anderson <joe@mousetrapped.co.uk>
2022-11-12 11:59:46 +0000

    Use more robust check for success

--- binding.rb
+++ binding.rb
@@ -1,11 +1,14 @@
 module MacOSPackager
   def self.package(build)
     destination_dir = File.join(__dir__, 'public', '.Pop Pixie.app')
+
     FileUtils.rm_rf(destination_dir)
     FileUtils.cp_r(Dir.glob(File.join(build.dir, '*.app')).first, destination_dir)
-    system File.join(__dir__, 'build-installer')
-    output_path = File.join(__dir__, 'Install Pop Pixie.dmg')
-    raise 'Failed to create macOS installer' unless File.exist?(output_path)
-    return output_path
+
+    unless system(File.join(__dir__, 'build-installer'))
+      raise 'Failed to create macOS installer'
+    end
+
+    return File.join(__dir__, 'Install Pop Pixie.dmg')
   end
 end
